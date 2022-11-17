464cc39cefbe9a1298b7bd30423bc98c8b666a32
Arthur Pastel <arthur.pastel@gmail.com>
2022-11-17 12:52:16 +0100

    feat: handle public uploads without a token

--- src/inputs.ts
+++ src/inputs.ts
@@ -2,9 +2,12 @@ import * as core from "@actions/core";
 import {ActionInputs} from "./interfaces";
 
 export const getActionInputs = (): ActionInputs => {
+  const token = core.getInput("token", {required: false});
+  const tokenless = token.length === 0;
   return {
-    token: core.getInput("token", {required: true}),
-    run: core.getInput("run", {required: true}),
     uploadUrl: core.getInput("upload_url", {required: true}),
+    run: core.getInput("run", {required: true}),
+    token,
+    tokenless,
   };
 };
