0268161e1b3fe86aa0507433e82374ac24df2caf
Arthur Pastel <arthur.pastel@gmail.com>
2022-11-17 12:52:16 +0100

    feat: handle public uploads without a token

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -2,9 +2,11 @@ export interface ActionInputs {
   token: string;
   uploadUrl: string;
   run: string;
+  tokenless: boolean;
 }
 
 export interface UploadMetadata {
+  tokenless: boolean;
   ref: string;
   headRef: string;
   baseRef: string;
@@ -12,6 +14,11 @@ export interface UploadMetadata {
   repository: string;
   commitHash: string;
   event: string;
+  profileMd5: string;
+  ghData: {
+    runId: number;
+    job: string;
+  };
 }
 
 export interface PostResponse {
--- src/upload.ts
+++ src/upload.ts
@@ -5,16 +5,34 @@ import * as fs from "fs";
 import {Readable} from "stream";
 import {context} from "@actions/github";
 import {TypedResponse} from "@actions/http-client/lib/interfaces";
+import crypto from "node:crypto";
+import md5File from "md5-file";
 
-const getUploadMetadata = (): UploadMetadata => ({
-  ref: context.ref,
-  headRef: context.payload?.pull_request?.head?.ref,
-  baseRef: context.payload?.pull_request?.base?.ref,
-  owner: context.repo.owner,
-  repository: context.repo.repo,
-  event: context.eventName,
-  commitHash: context.payload?.pull_request?.head?.sha ?? context.sha,
-});
+const getUploadMetadata = async ({
+  profilePath,
+  inputs,
+}: {
+  profilePath: string;
+  inputs: ActionInputs;
+}): Promise<UploadMetadata> => {
+  const hexMd5 = await md5File(profilePath);
+  const b64Md5 = Buffer.from(hexMd5, "hex").toString("base64");
+  return {
+    tokenless: inputs.tokenless,
+    ref: context.ref,
+    headRef: context.payload?.pull_request?.head?.ref,
+    baseRef: context.payload?.pull_request?.base?.ref,
+    owner: context.repo.owner,
+    repository: context.repo.repo,
+    event: context.eventName,
+    commitHash: context.payload?.pull_request?.head?.sha ?? context.sha,
+    profileMd5: b64Md5,
+    ghData: {
+      runId: context.runId,
+      job: context.job,
+    },
+  };
+};
 
 const createMultipartBody = (
   filePath: string
@@ -56,19 +74,29 @@ const upload = async (
   profilePath: string
 ): Promise<void> => {
   core.group("Upload Results", async () => {
-    const uploadMetadata = getUploadMetadata();
+    const uploadMetadata = await getUploadMetadata({profilePath, inputs});
     core.debug("Upload metadata:");
     core.debug(JSON.stringify(uploadMetadata, null, 2));
+    const hash = crypto
+      .createHash("sha256")
+      .update(JSON.stringify(uploadMetadata))
+      .digest("hex");
+    if (inputs.tokenless) {
+      core.info(`CodSpeed Run Hash: "${hash}"`);
+    }
 
     core.info("Preparing upload");
     let response: TypedResponse<PostResponse>;
     try {
+      const headers = inputs.tokenless
+        ? undefined
+        : {
+            Authorization: inputs.token,
+          };
       response = await http.postJson<PostResponse>(
         inputs.uploadUrl,
         uploadMetadata,
-        {
-          Authorization: inputs.token,
-        }
+        headers
       );
     } catch (e) {
       const err = e as httpm.HttpClientError;
@@ -83,14 +111,19 @@ const upload = async (
     const {multipartBody, boundary} = createMultipartBody(profilePath);
     core.info(`Uploading ${multipartBody.length} bytes...`);
     try {
-      await http.request(
+      const profile = fs.readFileSync(profilePath);
+      const uploadResponse = await http.request(
         "PUT",
         response.result.uploadUrl,
-        Readable.from(multipartBody),
+        Readable.from(profile),
         {
-          "Content-Type": `multipart/form-data; boundary=${boundary}`,
-          "Content-Length": multipartBody.length,
+          "Content-Type": "application/octet-stream",
+          "Content-Length": profile.length,
+          "Content-MD5": uploadMetadata.profileMd5,
         }
+      );
+      core.debug(
+        `Upload response: ${uploadResponse.message.statusCode} ${uploadResponse.message.statusMessage}`
       );
     } catch (e) {
       const err = e as httpm.HttpClientError;
