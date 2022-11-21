8aa08e13d6268622a7ecac85776b48bc392022a3
Arthur Pastel <arthur.pastel@gmail.com>
2022-11-21 12:57:10 +0100

    fix: improve error handling

--- src/prepare.ts
+++ src/prepare.ts
@@ -1,28 +1,29 @@
 import * as core from "@actions/core";
 import {exec} from "@actions/exec";
 
-const prepare = async (): Promise<void> =>
-  core.group("Prepare environment", async () => {
+const prepare = async (): Promise<void> => {
+  core.startGroup("Prepare environment");
+  try {
+    await exec("sudo apt-get install -y valgrind", [], {
+      silent: true,
+    });
     try {
-      await exec("sudo apt-get install -y valgrind", [], {
+      await exec("pip show pytest-codspeed", [], {
         silent: true,
       });
-      try {
-        await exec("pip show pytest-codspeed", [], {
-          silent: true,
-        });
-      } catch (e) {
-        core.warning(
-          "pytest-codspeed is not installed in your environment. Installing it..."
-        );
-        await exec("pip install pytest-codspeed", [], {
-          silent: true,
-        });
-      }
-    } catch (error) {
-      throw new Error(`Failed to prepare environment: ${error}`);
+    } catch (e) {
+      core.warning(
+        "pytest-codspeed is not installed in your environment. Installing it..."
+      );
+      await exec("pip install pytest-codspeed", [], {
+        silent: true,
+      });
     }
-    core.info("Environment ready");
-  });
+  } catch (error) {
+    throw new Error(`Failed to prepare environment: ${error}`);
+  }
+  core.info("Environment ready");
+  core.endGroup();
+};
 
 export default prepare;
