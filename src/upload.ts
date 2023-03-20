0f9a807aff8ad9112519c9e2ac62780d9d4b9c5b
Arthur Pastel <arthur.pastel@gmail.com>
2023-03-20 14:23:57 +0100

    fix: prefix the branch name with the fork name when relevant

--- src/upload.ts
+++ src/upload.ts
@@ -6,6 +6,7 @@ import * as tar from "tar";
 import {Readable} from "stream";
 import {context} from "@actions/github";
 import {TypedResponse} from "@actions/http-client/lib/interfaces";
+import type {PushEvent, PullRequestEvent} from "@octokit/webhooks-types";
 import crypto from "node:crypto";
 import md5File from "md5-file";
 
@@ -16,14 +17,25 @@ const getUploadMetadata = async ({
   profilePath: string;
   inputs: ActionInputs;
 }): Promise<UploadMetadata> => {
+  const pull_request = context.payload?.pull_request as
+    | PullRequestEvent["pull_request"]
+    | undefined;
+  const sender = context.payload?.sender as
+    | PullRequestEvent["sender"]
+    | PushEvent["sender"]
+    | undefined;
   const hexMd5 = await md5File(profilePath);
   const b64Md5 = Buffer.from(hexMd5, "hex").toString("base64");
+  const headRef =
+    pull_request?.head.repo.id === pull_request?.base.repo.id
+      ? pull_request?.head.ref
+      : `${pull_request?.head.repo.owner.login}:${pull_request?.head.ref}`;
   return {
     version: 1,
     tokenless: inputs.tokenless,
     ref: context.ref,
-    headRef: context.payload?.pull_request?.head?.ref,
-    baseRef: context.payload?.pull_request?.base?.ref,
+    headRef,
+    baseRef: pull_request?.base?.ref,
     owner: context.repo.owner,
     repository: context.repo.repo,
     event: context.eventName,
@@ -32,9 +44,9 @@ const getUploadMetadata = async ({
     ghData: {
       runId: context.runId,
       job: context.job,
-      sender: {
-        id: context.payload?.sender?.id,
-        login: context.payload?.sender?.login,
+      sender: sender && {
+        id: sender.id,
+        login: sender.login,
       },
     },
     runner: {
