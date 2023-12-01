de30d2b7e6b709ad7f06bee5a7bf2b4d5533ba09
Adrien Cacciaguerra <adrien.caccia@gmail.com>
2023-12-01 11:14:00 +0100

    chore: use full version in script and stop relying on pnpm

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# Usage: ./scripts/release.sh <major|minor|patch>
+# Usage: ./scripts/release.sh <version>
 set -ex
 
 if [ $# -ne 1 ]; then
-  echo "Usage: ./release.sh <major|minor|patch>"
+  echo "Usage: ./release.sh <version>"
   exit 1
 fi
 
@@ -13,17 +13,16 @@ if [ "$(git rev-parse --abbrev-ref HEAD)" != "main" ]; then
   exit 1
 fi
 git diff --exit-code
-pnpm format-check
-pnpm lint
-pnpm build
 
 # Bump version
-pnpm version $1 --git-tag-version=false
-pnpm build
-NEW_VERSION=$(pnpm version --json | jq '.["@codspeed/action"]' --raw-output)
+NEW_VERSION=$1
+# verify that NEW_VERSION is a valid semver
+if ! [[ "$NEW_VERSION" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+  echo "Version must be a valid semver (e.g. 1.2.3)"
+  exit 1
+fi
 MAJOR_VERSION=$(echo $NEW_VERSION | cut -d. -f1)
 
-git add package.json dist/index.js
 # Fail if there are any unstaged changes left
 git diff --exit-code
 git commit -m "Release v$NEW_VERSION 🚀"
